263502cd3b4beeb185e3cfb42aa28ae0ec63f069
sowjanya1308 <sowjanyasankara@gmail.com>
2024-01-05 21:41:00 +0530

    login

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const poppins = Poppins({ weight: ['100','200','300','400','500','600','700','800','900'], subsets:['latin']})
 
 export const metadata: Metadata = {
-  title: 'Instamart',
+  title: 'VoidMart',
   description: 'Online Grocery Delivery From Trusted Vendors',
 }
 
---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,25 @@
+import { Box, Button, Theme } from '@radix-ui/themes';
+import React, { useState } from 'react';
+// import { BsFillMoonStarsFill } from "react-icons/bs";
+
+export default function LoginPage(){
+
+    // const [change, setChange] = useState()
+
+    // const changeTheme = () => {
+    //     setChange()
+    // }
+
+    return (
+        <>
+        {/* <div>
+        <Button>
+            <BsFillMoonStarsFill/>
+        </Button>
+        </div> */}
+        <div>
+            hello
+        </div>
+        </>
+    );
+}
