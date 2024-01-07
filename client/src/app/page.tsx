e457f875a4aa4026904e46f31366f6572f0f704d
sowjanya1308 <sowjanyasankara@gmail.com>
2024-01-07 15:14:44 +0530

    carasoul

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,9 @@
 import Image from 'next/image'
 import {useAtomValue} from 'jotai'
 import { size } from '@/store/response'
+import { MobileHeader } from './components/header'
+import { Carousel } from './components/carousel'
+import { Box } from '@radix-ui/themes'
 
 export default function Home() {
   const mob = useAtomValue(size)
@@ -12,6 +15,20 @@ export default function Home() {
           hello bro
       </div>
     } 
+
+    {
+      (mob) && 
+      <>
+      <Box className='flex justify-between items-center p-3'>
+        <Box className='flex justify-between'>
+        <Image src={'/logo.png'} alt="logo" height={20} width={20}/>
+        <h3 className='font-semibold ml-2'>VoidMart</h3>
+        </Box>
+          <MobileHeader/>
+      </Box>
+      <Carousel/>
+      </>
+    }
     </>
   )
 }
