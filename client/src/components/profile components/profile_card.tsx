6bb2759faa6724e836af2f741b915e599b749629
Gomukha30 <sudutadikamalla@gmail.com>
2024-01-25 09:21:22 +0530

    latest changes

---
+++ client/src/components/profile components/profile_card.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { Avatar, Box, Container, ScrollArea, Flex, Text } from "@radix-ui/themes"
+import { useAtomValue, useSetAtom } from "jotai";
+import Image from "next/image";
+import { ItemCategory } from "@/store/response";
+
+export const MainProfile = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+
+export const MobileProfile = () =>{
+    return(
+        <>
+            
+        </>
+    )
+}
